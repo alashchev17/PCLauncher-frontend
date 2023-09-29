40ccf4e63fadf9ab61b08ebdf4d92dfcc087a029
alashchev17 <andrew.lashchev15@gmail.com>
2023-09-29 21:44:25 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,91 @@
-import { app } from 'electron';
-import { WebSocketConnection } from './websocket'
-import { autoUpdater } from 'electron-updater'
-import { SessionManager } from './session'
-import { ipcMain } from 'electron';
-import { Window } from './window';
-import { SettingsManager } from './settings';
-import * as path from 'path';
-
+import { app } from "electron";
+import { WebSocketConnection } from "./websocket";
+import { autoUpdater } from "electron-updater";
+import { SessionManager } from "./session";
+import { ipcMain } from "electron";
+import { Window } from "./window";
+import { SettingsManager } from "./settings";
+import * as path from "path";
 
 export class Main {
-    static isProduction : boolean = false;
+    static isProduction: boolean = false;
 
     static appData = path.join(app.getPath("appData"), app.getName());
-    
+
     static WS = new WebSocketConnection();
     static Session = new SessionManager();
-    static Initialized : boolean;
+    static Initialized: boolean;
     static Config = new SettingsManager();
-    static Logger = require('electron-log');
-    private updateChecked : boolean;
+    static Logger = require("electron-log");
+    private updateChecked: boolean;
     private IPCMethods = {
-        'login': (e:any, login: string, password: string, twofactor: number) =>{ Main.Session.authorize(login, password, twofactor); },
-        'logout': (e:any) => { Main.Session.logout(); },
-        'saveSettings': (e:any, data: any) => { Main.Config.updateSettings(data) }
-    }
+        login: (e: any, login: string, password: string, twofactor: number) => {
+            Main.Session.authorize(login, password, twofactor);
+        },
+        logout: (e: any) => {
+            Main.Session.logout();
+        },
+        saveSettings: (e: any, data: any) => {
+            Main.Config.updateSettings(data);
+        },
+    };
     constructor() {
         this.logger();
         this.init();
     }
     async init() {
         const startTime = performance.now();
-        
+
         for (const [key, value] of Object.entries(this.IPCMethods)) {
             ipcMain.on(key, value);
         }
-       
+
         Window.create();
 
         this.updater();
-        
+
         while (!Main.WS.isConnected() || !Window.DomLoad || !this.updateChecked) {
-            if(Main.WS.reconnectionCount == 0) {
+            if (Main.WS.reconnectionCount == 0) {
                 Main.WS.Reconnect();
             }
-            await new Promise(resolve => setTimeout(resolve, 500)); 
+            await new Promise(resolve => setTimeout(resolve, 500));
         }
         Main.Session.checkSession();
 
         Main.Config.send();
-        
+
         const endTime = performance.now();
-        Main.Logger.info(`[APP] Application initialized in ${endTime - startTime} ms`)
+        Main.Logger.info(`[APP] Application initialized in ${endTime - startTime} ms`);
         Main.Initialized = true;
     }
 
     updater() {
-        if(!Main.isProduction) {
+        if (!Main.isProduction) {
             this.updateChecked = true;
             return;
         }
 
         autoUpdater.autoDownload = true;
-          
+
         autoUpdater.checkForUpdates();
 
         Main.Logger.info("[UPDATES] Checking for updates");
 
-
-        autoUpdater.on('update-not-available', () => {
+        autoUpdater.on("update-not-available", () => {
             Main.Logger.info("[UPDATES] No updates");
             this.updateChecked = true;
-        });;
-        autoUpdater.on('update-downloaded', () => {
-            Main.Logger.info("[UPDATES] Installing updates");
-            autoUpdater.quitAndInstall()
         });
-        autoUpdater.on('error', (error) => {
+        autoUpdater.on("update-downloaded", () => {
+            Main.Logger.info("[UPDATES] Installing updates");
+            autoUpdater.quitAndInstall();
+        });
+        autoUpdater.on("error", error => {
             Main.Logger.error(error);
         });
     }
     logger() {
-        let format = '{d}.{m}.{y} {h}:{i}:{s} {text}';
-        Main.Logger.transports.file.level = true; 
+        let format = "{d}.{m}.{y} {h}:{i}:{s} {text}";
+        Main.Logger.transports.file.level = true;
         Main.Logger.transports.console.level = true;
 
         Main.Logger.transports.file.format = format;
@@ -95,5 +99,4 @@ export class Main {
 
         Main.Logger.catchErrors();
     }
-    
 }
